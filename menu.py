0b0fe7d58909556e48791943671de35f70833aa4
iDVal <Val.Demailly@gmail.com>
2018-08-18 18:37:38 +0200

    val

--- menu.py
+++ menu.py
@@ -11,14 +11,14 @@ canv = Canvas(menu, width=1200, height=675, bg='green')
 #c = Canvas(menu,)
 canv.pack()
 
-fond = PhotoImage(file="Fondzombie3.gif")
+fond = PhotoImage(file="images\Fondzombie3.gif")
 canv.create_image(600, 337.5, image=fond)
 
 
 # bouton cliquable (50, 50)
 #canv = Canvas(menu, width=128, height=128, bg='blue')
 #canv.pack()
-image_button = PhotoImage (file="quitter.gif")
+image_button = PhotoImage (file="images\quitter.gif")
 #canv.create_image(64, 64, image=image_button)
 bouton_quitter = Button(menu, image=image_button, command=menu.quit)
 bouton_quitter.pack()
